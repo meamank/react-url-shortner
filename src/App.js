0371c231c6ec704bb95faee559952ca30b2ecc04
Aman Kumar <meamank@gmail.com>
2021-10-17 20:36:37 +0530

    first deployment

--- src/App.js
+++ src/App.js
@@ -1,24 +1,75 @@
-import logo from './logo.svg';
-import './App.css';
+import { useRef, useState } from "react";
+import { Fragment } from "react/cjs/react.production.min";
+// import Logo from "./assets/logo.svg";
+import HeroImg from "./assets/hero.svg";
+import "./App.css";
 
 function App() {
+  const [shortLink, setShortLink] = useState(null);
+  const linkInputRef = useRef();
+  let links = [];
+  const submitHandler = async (event) => {
+    event.preventDefault();
+
+    const enteredLink = linkInputRef.current.value;
+
+    const response = await fetch(
+      `https://api.shrtco.de/v2/shorten?url=${enteredLink}`
+    );
+    const data = await response.json();
+
+    console.log(data);
+
+    if (data.ok === true) {
+      setShortLink(data.result);
+    }
+  };
+
+  if (shortLink && localStorage.getItem("link")) {
+    links.push(shortLink.full_short_link);
+  }
+  if (shortLink && !localStorage.getItem("link")) {
+    localStorage.setItem("link", shortLink.full_short_link);
+  }
+  console.log(links);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Fragment>
+      <div className="container">
+        <nav>
+          <p>Kutly</p>
+        </nav>
+        <main>
+          <div className="hero_header">
+            <h1>A simple and free URL shortner</h1>
+          </div>
+          <div className="hero">
+            <img src={HeroImg} alt="main hero" />
+          </div>
+          <div className="form">
+            <form onSubmit={submitHandler}>
+              <input
+                type="text"
+                placeholder="Paste your link here.."
+                ref={linkInputRef}
+              />
+              <button>Shorten it</button>
+            </form>
+          </div>
+          {shortLink && (
+            <div className="result">
+              <p className="result_item">{shortLink.full_short_link}</p>
+            </div>
+          )}
+          {/* {links &&
+            links.map((link) => (
+              <div className="saved_link">
+                <p className="result_item">{link}</p>
+              </div>
+            ))} */}
+        </main>
+      </div>
+    </Fragment>
   );
 }
 
