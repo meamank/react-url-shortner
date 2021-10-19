361f74f855608766a8b4067ae9ebae4e3ce0ed66
Aman Kumar <meamank@gmail.com>
2021-10-19 21:11:39 +0530

    added authentication

--- src/App.js
+++ src/App.js
@@ -1,75 +1,61 @@
-import { useRef, useState } from "react";
+import { useContext, useRef, useState } from "react";
 import { Fragment } from "react/cjs/react.production.min";
 // import Logo from "./assets/logo.svg";
 import HeroImg from "./assets/hero.svg";
 import "./App.css";
+import LoginForm from "./components/Login/LoginForm";
+import AuthContext from "./store/auth-context";
 
 function App() {
   const [shortLink, setShortLink] = useState(null);
   const [buttonText, setButtonText] = useState("Copy");
+  const [isShown, setIsShown] = useState(false);
   const linkInputRef = useRef();
-  let links = [];
+
+  const authCtx = useContext(AuthContext);
+
   const submitHandler = async (event) => {
     event.preventDefault();
 
     const enteredLink = linkInputRef.current.value;
 
-    // const response = await fetch("https://just.darshit.dev/shorten", {
-    //   body: value,
-    //   mode: "no-cors",
-    //   headers: {
-    //     "Content-Type": "application/json",
-    //   },
-    //   method: "POST",
-    //   redirect: 'follow'
-    // });
-    // const data = await response;
-
-    
-
-    var myHeaders = new Headers();
-    myHeaders.append("Accept", "*/*");
-
-    var raw = JSON.stringify({
-      url: enteredLink,
-    });
-
-    var requestOptions = {
-      method: "POST",
-      headers: myHeaders,
-      body: raw,
-      redirect: "follow",
-    };
-
-    fetch(
-      "https://53eb4870-2ef0-4458-9140-b7e9781e81a7.mock.pstmn.io/shorten",
-      requestOptions
-    )
+    fetch(`https://api.shrtco.de/v2/shorten?url=${enteredLink}`)
       .then((response) => response.json())
-      .then((result) => setShortLink(result))
+      .then((result) => setShortLink(result.result))
       .catch((error) => console.log("error", error));
   };
-
-
-
-  if (shortLink && localStorage.getItem("link")) {
-    links.push(shortLink.shortUrl);
-  }
-  if (shortLink && !localStorage.getItem("link")) {
-    localStorage.setItem("link", shortLink.shortUrl);
-  }
+  const showLoginHandler = () => {
+    setIsShown(true);
+  };
+  const closeLoginHandler = () => {
+    setIsShown(false);
+  };
+  const logoutHandler = () => {
+    authCtx.logout();
+  };
 
   const copyClipboard = (text) => {
-    navigator.clipboard.writeText(shortLink.shortUrl)
-    setButtonText(text)
-  }
-  console.log(links);
+    navigator.clipboard.writeText(shortLink.short_link);
+    setButtonText(text);
+  };
+
+  console.log(shortLink)
 
   return (
     <Fragment>
+      {isShown && <LoginForm onClose={closeLoginHandler} />}
       <div className="container">
         <div className="navbar">
           <p>Kutly</p>
+          {authCtx.isLoggedIn ? (
+            <button type="button" onClick={logoutHandler}>
+              Logout
+            </button>
+          ) : (
+            <button type="button" onClick={showLoginHandler}>
+              Login
+            </button>
+          )}
         </div>
         <section>
           <main>
@@ -97,7 +83,7 @@ function App() {
             </div>
             {shortLink && (
               <div className="result">
-                <p className="result_item">{shortLink.shortUrl}</p>
+                <p className="result_item">{shortLink.short_link}</p>
                 <button
                   className="copy"
                   onClick={() => copyClipboard("copied")}
---
+++ src/components/Login/LoginForm.js
@@ -0,0 +1,112 @@
+import { useContext, useRef, useState } from "react";
+import AuthContext from "../../store/auth-context";
+import Modal from "../UI/Modal";
+import classes from "./Login.module.css";
+
+const LoginForm = (props) => {
+
+  const [isLogin, setIsLogin] = useState(true)
+  const [error, setError] = useState(false)
+  const [isLoading, setIsLoading] = useState(false)
+
+  const emailInputRef = useRef()
+  const passwordInputRef = useRef()
+
+  const authCtx = useContext(AuthContext)
+
+  const switchAuthModeHandler = () => {
+    setIsLogin((prevState) => !prevState);
+  }
+
+  const submitHandler = async (event) => {
+    event.preventDefault()
+
+    const enteredEmail = emailInputRef.current.value;
+    const enteredPassword = passwordInputRef.current.value;
+
+    let url;
+
+    if (isLogin) {
+      url = "https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyATohObT48z3aw1doZCzzYiprwu03GLeSA"
+    } else {
+      url = "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyATohObT48z3aw1doZCzzYiprwu03GLeSA"
+    }
+    fetch(
+      url,
+      {
+        method: "POST",
+        body: JSON.stringify({
+          email: enteredEmail,
+          password: enteredPassword,
+          returnSecureToken: true,
+        }),
+        headers: {
+          "content-type": "application/json",
+        },
+      }
+    ).then((res) => {
+      setIsLoading(false);
+      if (res.ok) {
+        return res.json()
+      } else {
+        return res.json().then((data) => {
+          // let errorMessage = "Authentication failed"
+          throw new Error(data.error.message)
+        });
+      }
+    })
+    .then((data) => {
+      const expirationTime = new Date((new Date().getTime() + (+data.expiresIn * 1000)))
+      authCtx.login(data.idToken, expirationTime)
+      // history.replace('/')
+    }).catch((err) => {
+      // console.log(err)
+      setError(err.message)
+    })
+  };
+
+
+
+
+  return (
+    <>
+    {!authCtx.isLoggedIn && <Modal onClose= {props.onClose}>
+      <div className={classes.auth}>
+        <h1>{isLogin ? "Login" : "Sign Up"}</h1>
+        <form onSubmit={submitHandler}>
+          <div className={classes.control}>
+            <label htmlFor="email">Your Email</label>
+            <input type="email" id="email" required ref={emailInputRef} />
+          </div>
+          <div className={classes.control}>
+            <label htmlFor="password">Your Password</label>
+            <input
+              type="password"
+              id="password"
+              required
+              ref={passwordInputRef}
+            />
+          </div>
+          {error && <p>{error}</p>}
+
+          <div className={classes.actions}>
+            {!isLoading && (
+              <button>{isLogin ? "Login" : "Create Account"}</button>
+            )}
+            {isLoading && <p>sending....</p>}
+            <button
+              type="button"
+              className={classes.toggle}
+              onClick={switchAuthModeHandler}
+            >
+              {isLogin ? "Create new account" : "Login with existing account"}
+            </button>
+          </div>
+        </form>
+      </div>
+    </Modal>}
+    </>
+  );
+};
+
+export default LoginForm;
